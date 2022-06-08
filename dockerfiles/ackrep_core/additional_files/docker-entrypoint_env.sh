61f23a78d16adcfa951ba12badf57c11c2f158cb
JuliusFiedler <ichderjulius@gmx.de>
2022-06-08 12:13:05 +0200

    fix: prevent old db files from causing issues -> rm fix: load db correctly

--- dockerfiles/ackrep_core/additional_files/docker-entrypoint_env.sh
+++ dockerfiles/ackrep_core/additional_files/docker-entrypoint_env.sh
@@ -3,12 +3,17 @@ set -e
 
 # create an empty database
 python manage.py migrate --noinput --run-syncdb >/dev/null
-
+# echo $ACKREP_DATABASE_PATH
 if [[ $ACKREP_DATABASE_PATH == *"unittest"* ]]
 then
-    python -c "from ackrep_core import core; core.load_repo_to_db('/code/ackrep_data_for_unittests')" >/dev/null
-else
+    echo "loading unittest repo"
+    python -c "from ackrep_core import core; core.load_repo_to_db('/code/ackrep_data_for_unittests')" #>/dev/null
+elif [[ $ACKREP_DATABASE_PATH == *"db"* ]]
+then
+    # echo "loading data repo"
     python -c "from ackrep_core import core; core.load_repo_to_db('/code/ackrep_data')" >/dev/null
+else
+    echo "no db path specified, no db loaded"
 fi
 
 exec "$@"
