f9d12229e8b264e903dc86f1e1deeaed4d43ff49
Carsten Knoll <carsten.knoll@posteo.de>
2023-05-11 21:05:07 +0200

    make docker setup run locally

--- dockerfiles/ackrep_core/additional_files/docker-entrypoint.sh
+++ dockerfiles/ackrep_core/additional_files/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
 
     # create an empty database
-    python manage.py migrate --noinput --run-syncdb #>/dev/null
+    ackrep --bootstrap-db
     python -c "from ackrep_core import core; core.load_repo_to_db('/code/ackrep/ackrep_data')" #>/dev/null
 
     # this is the place where fixtures could be loaded
